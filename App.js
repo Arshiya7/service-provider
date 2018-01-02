5272f4d80cd315c3457820b6eaa2b7e665195189
Arshiya7 <arshiya.b@appliedsni.com>
2018-01-02 14:53:11 +0530

    firebase added

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
+import firebase from 'firebase';
 import Login from './src/components/login/Login';
 import Router from './src/components/common/Router';
 import Header from './src/components/Header';
+
+
+const config = {
+  apiKey: 'AIzaSyATYNuxqSkdzdsxu82W5aZHuTm5qVjcwPM',
+  authDomain: 'serviceprovider-c6bcb.firebaseapp.com',
+  databaseURL: 'https://serviceprovider-c6bcb.firebaseio.com',
+  projectId: 'serviceprovider-c6bcb',
+  storageBucket: 'serviceprovider-c6bcb.appspot.com',
+  messagingSenderId: '937556230769'
+};
+
+const firebaseApp = firebase.initializeApp(config);
 
 class App extends Component {
    render() {
